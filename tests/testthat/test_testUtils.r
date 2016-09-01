da6304264a688d79fed3f4b59a2391a622c1b6b6
Stuart R. Jefferys <srjefferys@gmail.com>
2016-09-01 17:45:42 -0400

    FEAT: Add test for equality that allows NULL.
    For use with testthat testing framework.

--- tests/testthat/test_testUtils.r
+++ tests/testthat/test_testUtils.r
@@ -39,3 +39,21 @@ describe( "Checking conditions with expect_condition", {
       expect_failure( expect_condition( Exception(call=NULL), "Exception" ), wantRE )
    })
 })
+describe( "Checking equality including null with expect_equalOrNull", {
+	aList <- list(a=1, b=2)
+	bList <- list(b=2, a=1)
+	it( "Works like expect_equal if 'want' is not null", {
+		expect_success(expect_equalOrNull( 1, 1 ))
+		expect_success(expect_equalOrNull( aList, aList ))
+
+		expect_failure(expect_equalOrNull( aList, bList ))
+		expect_failure(expect_equalOrNull( 1, "1" ))
+		expect_failure(expect_equalOrNull( NULL, 1 ))
+
+	})
+	it( "Works like is_null if 'want' is null", {
+		expect_success( expect_equalOrNull( aList$c, NULL ))
+		expect_failure( expect_equalOrNull( aList["c"], NULL ))
+		expect_failure(expect_equalOrNull( 1, NULL ))
+	})
+})
