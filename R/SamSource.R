fab4114903b36aacfca1b617793009f451e435a2
Stuart R. Jefferys <srjefferys@gmail.com>
2016-09-14 11:32:47 -0400

    FEAT: Implement minimal SamSource object.
    Minimal object and property accessors. Not fully tested but generally working.

--- R/SamSource.R
+++ R/SamSource.R
@@ -1,14 +1,164 @@
 # samSource.R - Construct and work with source data *.sam file
 
+#' Create or retrieve \code{SamSource} objects.
+#'
+#' A \code{SamSource} object is meant to be an opaque object used to store the
+#' source information associated with a Sam file. Currently only \code{type=
+#' "file"} is supported, but \code{"url"} may be added. The name, host machine
+#' name, and type can be retrieved from \code{SamSource} or \code{Sam} objects
+#' with \code{samSourceName()}, \code{samSourceHost()}, and
+#' \code{samSourceType()}, respectively. A \code{SamSource} object can be
+#' retrieved from a \code{Sam}, \code{SamHeader}, or \code{SamReads} object
+#' using \code{SamSource()}.
+
+#' @param x The object to construct a SamSource object from. Can be a Sam,
+#'   SamHeader, or SamReads object, a list, or the name of the source file/url
+#'   as a character vector, or an explicit NULL. If this is a list ,
+#'   \code{"name="}, \code{"host="} and \code{"type="} elements can be specified
+#'   as character vectors. Missing or NULL list elements are treated as N/A. All
+#'   character vectors should be the same length. If this is an explicit
+#'   \code{NULL}, it is treated the same as if it was an empty list.
+#' @param host The name of the system the sam file is available on. Default is
+#'   \code{NA}.
+#' @param type The type of source for the sam file. Currently only "file" is
+#'   supported. "url" may be added. The default is NA.
+#' @param ... Required for S3 object method implementation. Not currently used.
+#'
+#' @return Returns a SamSource object, an opaque object from which the source
+#'   file name, host machine, and type of access can be extracted.
+#'
+#' @examples
+#' \dontrun{
+#'   sam <- Sam("file.sam")
+#'   stopIfNot( SamSource( sam ) == SamSource( SamHeader( sam ) ==
+#'   	SamSource( SamReads( sam )))
+#'
+#'   source2 <- SamSource( "file.sam", host="bioinf.unc.edu", type="file")
+#'
+#'   sourceList <- list(name="file.sam", host="bioinf.unc.edu", type="file")
+#'   source3 <- SamSource( sourceList )
+#'
+#'   source4 <- SamSource(NULL)
+#' }
 #' @export
 SamSource <- function (x, ...) {
 	UseMethod("SamSource", x)
 }
 
+#' @rdname SamSource
 #' @export
-SamSource.Sam <- function ( x ) {
+SamSource.character <- function ( x, host=NA, type=NA, ... ) {
+	return( structure(
+		class="SamSource",
+		list( name=x, host=host, type=type )
+	))
+}
+
+#' @rdname SamSource
+#' @export
+SamSource.list <- function ( x, ... ) {
+	return( structure(
+		class="SamSource",
+		list( name=x$name, host=x$host, type=x$type )
+	))
+}
+
+#' @rdname SamSource
+#' @export
+SamSource.Sam <- function ( x, ... ) {
 	return( structure(
 		class="SamSource",
 		attr(x, "source")
 	))
 }
+
+#' @rdname SamSource
+#' @export
+SamSource.NULL <- function ( x, ... ) {
+	return( structure(
+		class="SamSource",
+		list( name=NA, host=NA, type=NA )
+	))
+}
+
+#' SamSource name data accessor
+#'
+#' Accessor to extract the name of the sam file data was taken from.
+#'
+#' @param x Can be a Sam or SamSource object.
+#'
+#' @return The name of the source sam data file as a character vector.
+#'
+#' @export
+samSourceName <- function (x, ...) {
+	UseMethod("samSourceName", x)
+}
+
+#' @rdname samSourceName
+#' @export
+samSourceName.SamSource <- function( x, ... ) {
+	return(x$name)
+}
+
+#' @rdname samSourceName
+#' @export
+samSourceName.Sam <- function( x, ... ) {
+	return(x$source$name)
+}
+
+#' SamSource host data accessor
+#'
+#' Accessor to extract the name of the host machine where the sam file the data
+#' was taken from resides.
+#'
+#' @param x Can be a Sam or SamSource object.
+#'
+#' @return The name name of the host machine where the sam file the data was
+#'   taken from resides as a character vector.
+#'
+#' @export
+samSourceHost <- function (x, ...) {
+	UseMethod("samSourceHost", x)
+}
+
+#' @rdname samSourceHost
+#' @export
+samSourceHost.SamSource <- function( x, ... ) {
+	return(x$host)
+}
+
+#' @rdname samSourceHost
+#' @export
+samSourceHost.Sam <- function( x, ... ) {
+	return(x$source$host)
+}
+
+#' SamSource data type accessor
+#'
+#' Accessor to extract the type of access required for the sam file the data was
+#' taken from resides.
+#'
+#' @param x Can be a Sam or SamSource object.
+#'
+#' @return The type of access required for the sam file the data was taken from,
+#'   as a character vector. Can only be "file" currently. "url" is a likely
+#'   future extension.
+#'
+#' @export
+samSourceType <- function (x, ...) {
+	UseMethod("samSourceType", x)
+}
+
+#' @rdname samSourceType
+#' @export
+samSourceType.SamSource <- function( x, ... ) {
+	return(x$type)
+}
+
+#' @rdname samSourceType
+#' @export
+samSourceType.Sam <- function( x, ... ) {
+	return(x$source$type)
+}
+
+
--- tests/testthat/test_SamSource.r
+++ tests/testthat/test_SamSource.r
@@ -2,6 +2,23 @@ context("Testing the SamSource object functionality")
 
 samFile <- file.path( "goodSamFiles", "pe.sam" )
 
+describe( "SamSource() constructs create correct objects", {
+	samSourceData <- list(name="aSourceName", type="file", host="bioinfo.unc.edu")
+	it( "Works for list input", {
+		obj <- SamSource( samSourceData )
+		expect_equal(samSourceName(obj), samSourceData$name )
+		expect_equal(samSourceHost(obj), samSourceData$host )
+		expect_equal(samSourceType(obj), samSourceData$type )
+	})
+	it( "Works for character input", {
+		obj <- SamSource( samSourceData$name,
+								host= samSourceData$host, type= samSourceData$type )
+		expect_equal(samSourceName(obj), samSourceData$name )
+		expect_equal(samSourceHost(obj), samSourceData$host )
+		expect_equal(samSourceType(obj), samSourceData$type )
+	})
+})
+
 describe( "Test fixtures for use in the test_sam testthat file", {
 	it( "Provides 'samFile' as a generic input sam file.", {
 		expect_true( file.exists( samFile ))
