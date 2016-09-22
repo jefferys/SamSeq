a90d354b80f8230fb7d8bfd23ff8cc83c1e156b4
Stuart R. Jefferys <srjefferys@gmail.com>
2016-09-22 18:39:39 -0400

    ADD Starting development of viral integration script.

---
+++ exec/viralSite.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+library("SamSeq")
+
+# Helper function for getSettings(). Converts arguments to option values,
+# validates the options (possibly modifying them in the process), and either
+# exits with error or return the resulting list of options.
+
+viralSite()
