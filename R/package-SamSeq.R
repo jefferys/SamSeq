dab6eb5bf9496dcc7c74b14ed6a229ea52b64ff6
Stuart R. Jefferys <srjefferys@gmail.com>
2016-09-25 13:14:00 -0400

    DEV: Split out packages Exception and JefferysRUtils.
    Deleted code now in these packges, created and published these packages to GitHub, added package documentation as place to import them.

---
+++ R/package-SamSeq.R
@@ -0,0 +1,13 @@
+#' Work with aligned reads from a SAM file
+#'
+#' Work with the aligned read data as provided in a *.sam file, as in the format
+#' used by SamTools. Tracks header information, reads, and the source file.
+#' Loads all reads and optionally the extra read tags. Includes functions to
+#' translate the Sam flags and to filter reads.
+#'
+#' @name package-SamSeq
+#'
+#' @import Exception
+#' @importFrom JefferysRUtils %p% %pp% expect_equalOrNull
+NULL
+# NULL
