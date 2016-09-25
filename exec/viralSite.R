3790841c6e47d56d0f55194c0d907dca1ebef776
Stuart R. Jefferys <srjefferys@gmail.com>
2016-09-25 13:39:16 -0400

    DEV:: Spliting out ViralSiteApp to new project.
    Deleting vignette, not relevant after several iterations.

--- exec/viralSite.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-library("SamSeq")
-
-# Helper function for getSettings(). Converts arguments to option values,
-# validates the options (possibly modifying them in the process), and either
-# exits with error or return the resulting list of options.
-
-viralSite()
