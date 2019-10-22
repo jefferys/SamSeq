cac87b28abb356eeac0aced09d24fbc3b80075b4
Stuart R Jefferys <srj@unc.edu>
2019-10-22 20:26:32 +0000

    DOC: Update documentation.

--- R/SamReads.R
+++ R/SamReads.R
@@ -5,29 +5,23 @@
 #' S3 method that returns an S3 SamReads object, which is a data frame with
 #' additional meta data.
 #'
-#' @param x For \code{SamHeader()}, the object to extract or build a sam reads
-#'   object from. It can be a character vector of unparsed read lines, or
-#'   a \code{Sam} object. For \code{as.data.frame()}, it can only be a
-#'   \code{SamReads} object.
+#' @param x For the object to extract or build a sam reads object from. It can
+#'   be a character vector of unparsed read lines, a \code{Sam} object, or
+#'   (pretty uselessly) a \code{SamReads} object.
 #'
-#' @param source The SamSource object to associate with the reads. If not
-#'   provided and \code{x} is a \code{Sam} object, it will just use its source.
-#'   For other methods, the default is all missing data, i.e.
-#'   \code{SamSource(NULL)}
+#' @param source The \code{SamSource} object to associate with the reads. If
+#'   not provided and \code{x} is a \code{Sam} or \code{SamReads} object, it
+#'   will default to that object's source. Otherwise the default is all missing
+#'   data, i.e. \code{SamSource(NULL)}
 #'
 #' @param splitTags By default this is \code{TRUE}, meaning optional read tags
 #'   are split out as named columns. Setting this \code{FALSE} leaves the
-#'   optional tags as a single column, \code{tags}, with all optional tags
+#'   optional tags as a single column, "tags", with all the extra tags
 #'   presented as a single tab delimited string.
 #'
-#' @param ... Required for S3 object method implementation. Not currently used.
+#' @param ... Future-proof the method. Not currently used.
 #'
-#' @return For \code{SamReads}, a \code{SamReads} object.
-#'
-#'   For \code{as.data.frame} a data frame as returned by
-#'   \code{parseSamReadLines}, with either parsed tags or a tags field,
-#'   whichever was used to create the SamReads object. Can use
-#'   \code{isTagsParsed(SamHeader)} to check.
+#' @return A \code{SamReads} object.
 #'
 #' @export
 SamReads <- function (x, ...) {
@@ -36,20 +30,24 @@ SamReads <- function (x, ...) {
 
 #' Parse the optional sam read tags into a data frame
 #'
-#' @param tagList An un-named list with one element per read line, in order, where each
-#' element is an un-named list, one element per field, where each field after a
-#' given column (by default 12) is a key:typeFlag:value optional sam tag. Order
-#' of the tags in the list is unimportant (although it does affect the order
-#' of the tags in the returned data frame).
+#' @param tagList An un-named list with one element per read line, in order. Each read line
+#' element is itself an un-named list with one element per sam record field. Each field from a
+#' given column (by default 12) must be an optional sam tag formatted as key:typeFlag:value. Only these optional sam tags are parsed. If only optional tags are present, can set \code{startAt= 1}. The
+#' order of optional tags as first encountedered determines the order reported.
 #'
 #' @param startAt The first column in the field list that is to be parsed. All
 #' columns after this are assumed to be optional tag fields. By default the
-#' 12'th column is the start of the optional tags.
+#' 12'th column is the start of the optional tags; this is the default full sam record.
 #'
 #' @return A data frame with one column per two character parsed tag. Values are
 #' converted to integer if typeFlag is i and to double if typeFlag is f.
 #' Otherwise left as character. Any tag that is found only on some lines will
 #' have a value of NA on any line where it is not found.
+#'
+#' @section Messages:
+#'
+#' As this takes a long time, intermediate messages are printed to STDOUT
+#' while running.
 #'
 parseSamReadTags <- function( tagList, startAt= 12 ) {
 
